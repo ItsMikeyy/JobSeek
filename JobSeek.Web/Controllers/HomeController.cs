f612ccab2accd03ea02fae58f9cbf52bc250a9ed
Michael Carey <michaelcarey14@outlook.com>
2026-02-22 18:21:36 -0700

    commit

--- JobSeek.Web/Controllers/HomeController.cs
+++ JobSeek.Web/Controllers/HomeController.cs
@@ -17,6 +17,11 @@ namespace JobSeek.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
